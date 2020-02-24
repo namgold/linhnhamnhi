24cc06b8e2acbaa357cb1dcd6e8e2a873bfd868a
Nam Nguyen <hnamnguyencse@gmail.com>
2020-02-24 16:55:02 +0700

    controller

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res) {
+    res.json({
+        frontendMessage: req.content,
+        message: "Nam yêu Linh heo nhiều lắm nè"
+    })
 });
 
 module.exports = router;
