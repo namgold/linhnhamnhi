9e451c1e2626014d0c80bab752dbc3ca0a91f880
Linh Hy <hyphamngoclinh99@gmail.com>
2020-02-24 18:05:05 +0700

    fix ajax

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 
-function sendContent(){
+function sendContent(e) {
+    e.preventDefault();
+    var form = $(this);
     $.ajax({
         url: "/", 
         method: "POST",
-        data: {content: content.value},
+        data: form.serialize(),
         success: function(result){
             $("#frontendMessage").html(result.frontendMessage);
             $("#message").html(result.message);
